106ad6f117210047e1b36e7d0e5b0fcf132e226c
Chenjae <injea95@naver.com>
2021-10-27 09:12:43 +0900

    21-10-26 수업 내용

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="card">
+    <div class="card-header">
+      About
+    </div>
+    <div class="card-body">
+      <div class="about">
+        This is an about page
+      </div>
+    </div>
   </div>
 </template>
+<!-- 위의 template 내용은 모두 javaScript로 변환된다. 따라서 안에 자바스크립트 변수를 사용할 수 있다 -->
+
+<!-- vue파일을 module로 만든다. exprot default 안해도 자동으로 생성 -->
+<script>
+export default {
+  name : "About"
+}
+</script>
