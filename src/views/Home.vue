57052397c171b3ec356f8f34e7c776c837635a97
Chenjae <injea95@naver.com>
2021-10-27 09:12:43 +0900

    21-10-28 수업 내용

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="card">
+    <div class="card-header">
+      HOME
+    </div>
+    <div class="card-body">
+      <div class="home">
+        <img alt="Vue logo" src="@/assets/logo.png" />
+        <hello-world/> <!-- template으로 정의한 내용이 javaScript로 변환되서 대입된다 -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
