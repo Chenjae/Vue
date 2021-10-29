b532ffab6b31aaa62619109e7469044030875157
Chenjae <injea95@naver.com>
2021-10-29 09:13:29 +0900

    21-10-28 수업 내용

--- src/views/menu02/Exam02Directives.vue
+++ src/views/menu02/Exam02Directives.vue
@@ -6,12 +6,25 @@
         </div>
         <div class="card-body">
             <div>
-                <img class="mr-2" src="@/assets/photos/photo1.jpg" height="300"/>
+                <img class="mr-2" src="@/assets/photos/photo1.jpg" height="250"/>
                 <!-- v-bind로 데이터의 경로를 완성할 경우에는 require 함수를 사용해야한다. -->
-                <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="300"/>
+                <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="250"/>
             </div>
             <hr/>
+            <!-- 요소 숨김 여부 -->
+            <span v-if="sale" class="mr-2">
+                <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="250"/>
+            </span>
+            <span v-show="sale">
+                <img v-bind:src="require(`@/assets/photos/${photoFileName}`)" height="250"/>
+            </span>
+            <!-- HTML 태그 처리 -->
+            <p>정보: {{info}}</p>
+            <p>정보: <span v-html="info"></span></p>
+
+            <hr/>
             <button class="btn btn-sm btn-info" @click="changeData">변경</button>
+
         </div>
     </div>    
 </template>
@@ -22,11 +35,14 @@ export default {
     name: "Exam02Directives",
     //추가하고 싶은 컴포넌트 등록
     components: {
+
     },
     //컴포넌트 데이터 정의
     data: function() {
         return {
-            photoFileName: "photo2.jpg"
+            photoFileName: "photo2.jpg",
+            sale: true,
+            info: `<div style="background-color: #eeeeee">스타일 적용 확인</div>`
         };
     },
     //컴포넌트 메소드 정의
@@ -37,6 +53,7 @@ export default {
             } else {
                 this.photoFileName = "photo2.jpg"
             }
+            this.sale = !this.sale;
         }
     }
 }
